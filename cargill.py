49b1a3540ec5d98ec3a4fcf22d16a903b15ebf11
solutionlog2024 <analisedadossolution@gmail.com>
2024-10-17 00:05:03 -0300

    Update cargill.py

--- cargill.py
+++ cargill.py
@@ -25,7 +25,7 @@ st.markdown('''
 ''')
 
 # Input de data para filtrar o DataFrame
-data_selecionada = st.date_input("Selecione uma data", value=pd.to_datetime().date())
+data_selecionada = st.date_input("Selecione uma data", value=pd.Timestamp.now().date()
 
 # Filtrando o DataFrame pela data selecionada
 df_filtrado = df[df['DATA'].dt.date == data_selecionada]
